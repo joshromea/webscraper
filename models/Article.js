5588fea093f88c4702c93659336717737ed1790b
Josh Romea <josh.romea@gmail.com>
2019-03-05 23:40:54 -0700

    updated article model

--- models/Article.js
+++ models/Article.js
@@ -15,6 +15,10 @@ const ArticleSchema = new Schema({
         type: String,
         required: false
     },
+    image: {
+        type: String,
+        required: false
+    },
     note: {
         type: Schema.Types.ObjectId,
         ref: "Note"
